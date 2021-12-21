7d524d3dcbb4778badf8b05ff80e9aa47614dcb8
Robertson Akpan <robertsonakpan@gmail.com>
2021-12-21 19:29:24 +0100

    New files {selectPlayer module, gameTitle module, event renamed to gameLogic} added

---
+++ src/components/gameTitle.js
@@ -0,0 +1,16 @@
+
+  const gameTitle = (() => {
+    const gameName = document.querySelector(".game-name");
+    console.log('I am the Title')
+
+
+    return { gameName }
+  })()
+  
+  
+
+
+
+
+
+  export { gameTitle }
